bfefc803dbd567bddb12448801ae918235e8f888
Bryan M Lenihan <blenihan@tiberhealth.com>
2023-12-08 13:29:14 -0500

    Long Description

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author='Tiber Health Innovations',
     author_email='tiber-tools@tiberhealth.com',
     description='Canvas Data 2 Pull Routine',
+    long_description='Canvas Data 2 Pull Routine by Tiber Health Innovations. Script is used to pull data from the CanvasData2 portals and create sql and load files for MySQL version 8.x',
     install_requires=[
         "asyncio",
         "instructure-dap-client",
