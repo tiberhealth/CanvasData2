2f763d2ee48dcac4e5d36045662187bddd31d6a6
Bryan M Lenihan <blenihan@tiberhealth.com>
2023-12-08 14:19:36 -0500

    Trying to get it working

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cd2datamanager',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     url='https://github.com/tiberhealth/CanvasData2',
     license='',
