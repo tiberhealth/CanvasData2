148fddac5e27f1d7b4b427e2586a3d1e68896143
Bryan M Lenihan <blenihan@tiberhealth.com>
2023-12-08 13:52:19 -0500

    Setting up package

--- cd2datamanager/main.py
+++ cd2datamanager/main.py
@@ -1,5 +1,4 @@
 import asyncio
-import sys
 import typer
 
 import constants
--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from setuptools import setup
 from setuptools import setup, find_packages
 
 setup(
-    name='tibercanvasdata2',
+    name='cd2datamanager',
     version='1.0',
     packages=find_packages(),
     url='https://github.com/tiberhealth/CanvasData2',
     license='',
     author='Tiber Health Innovations',
     author_email='tiber-tools@tiberhealth.com',
-    description='Canvas Data 2 Pull Routine',
-    long_description='Canvas Data 2 Pull Routine by Tiber Health Innovations. Script is used to pull data from the CanvasData2 portals and create sql and load files for MySQL version 8.x',
+    description='Canvas Data 2 Data Manager',
+    long_description='Canvas Data 2 Data Manager by Tiber Health Innovations. Script is used to pull data from the CanvasData2 portals and create sql and load files for MySQL version 8.x',
     install_requires=[
         "asyncio",
         "instructure-dap-client",
